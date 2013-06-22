067d063fcc4fe6835a315930144208e7a6e7ba53
jishi jeici <jishi.jeici@gmail.com>
2013-06-21 22:07:15 -0600

    Revert "proyecto eclipse"
    This reverts commit 68a708423b401ba3cd74a3fc15b466124f0eae17.

--- gen/sv/edu/fia/ues/siatg/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package sv.edu.fia.ues.siatg;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/sv/edu/fia/ues/siatg/Ttabajograduacion.java
+++
@@ -1,5 +0,0 @@
-package sv.edu.fia.ues.siatg;
-
-public class Ttabajograduacion {
-
-}
